45e4cb959ddaa9066c56f630674678609be786a7
seominb <zyuqw1193@naver.com>
2021-05-26 01:04:40 -0700

    90% 완료

--- calc.h
+++ calc.h
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 /************************************************
  * calc.c에 필요한 헤더파일들은 현재 파일에 선언
  * calc.c에 구현될 함수들의 원형(proto type)은 현재 파일에 선언
@@ -6,3 +8,35 @@
  * ************************************************/
 
 void calc(void);
+char* transformation(char * input);
+char* postfix(char * transformed);
+void input(void);
+void deleteSpace(char *);
+void calculate(char *postfixed);
+
+_Bool isEmpty();
+void push(char operator);
+char pop();
+char peek();
+
+double calStack[100]={};
+int calTop=-1;
+
+_Bool calIsEmpty(){
+	return (calTop==-1);
+}
+
+double calPeek(){
+	if(!calIsEmpty()){
+		return calStack[calTop];
+	}
+}
+
+double calPush(char operator){
+	calStack[++calTop]=operator;
+}
+double calPop(){
+	if(!(calIsEmpty())){
+		return calStack[calTop--];
+	}
+}
